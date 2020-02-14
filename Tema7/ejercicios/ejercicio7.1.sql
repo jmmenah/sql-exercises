158d28c91556b6d24e0e6f439704d58ae18b5472
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-02-14 17:03:57 +0100

    Update ejercicio7.1.sql

--- Tema7/ejercicios/ejercicio7.1.sql
+++ Tema7/ejercicios/ejercicio7.1.sql
@@ -12,10 +12,13 @@ SELECT NIF, Descripción, Fecha, Nota FROM Matrícula,Asignatura WHERE (Código=
 SELECT Nombre AS "Nombre" ,Apellido1 AS "1º Apellido" ,Apellido2 AS "2º Apellido" FROM Alumno WHERE Cod_Localidad='14049';
 --7. Muestra los datos de todas las asignaturas de 2-ESO, 3-ESO y 4-ESO
 SELECT * FROM Asignatura WHERE Curso="4-ESO" OR Curso="3-ESO" OR Curso="2-ESO";
+SELECT * FROM Asignatura WHERE Curso IN("4-ESO","3-ESO","2-ESO");
 --8. Muestra los datos de todas las asignaturas cuyo código esté entre 3 y 6
 SELECT * FROM Asignatura WHERE Código>=3 AND Código<=6;
+SELECT * FROM Asignatura WHERE Código BETWEEN 3 AND 6;
 --9. Obtén un listado de todos los alumnos, uniendo en una sola columna el nombre y los dos apellidos
 SELECT NIF,CONCAT(Nombre,' ', Apellido1,' ', Apellido2) AS "Nombre Completo", Dirección, Telefono FROM Alumno;
+SELECT NIF,CONCAT_WS(' ',Nombre, Apellido1, Apellido2) AS "Nombre Completo", Dirección, Telefono FROM Alumno;
 --10. Obtén un listado con todas las asignaturas que tengan algún departamento asignado
 SELECT * FROM Asignatura WHERE Departamento IS NOT NULL;
 --11. Selecciona todos los alumnos cuyo primer apellido termine en 'a'
@@ -24,9 +27,10 @@ SELECT * FROM Alumno WHERE Apellido1 LIKE '%a';
 SELECT * FROM Alumno WHERE Apellido1 LIKE 'M%a';
 --13. Selecciona todos los alumnos cuyo primer apellido termine en 'a' y tengan 9 caracteres
 SELECT * FROM Alumno WHERE Apellido1 LIKE '%a' AND LENGTH(Apellido1)=9;
+SELECT * FROM Alumno WHERE Apellido1 LIKE '________%a';
 --14. Devuelve los datos de los dos primeros alumnos
 SELECT * FROM Alumno LIMIT 2;
 --15. Obtén un listado de todas las asignaturas ordenadas por descripción en orden ascendente y por curso en orden descendente
 SELECT * FROM Asignatura ORDER BY Descripción ASC, Curso DESC;
 --16. Devuelve los datos de las tres asignaturas a partir de la segunda
-SELECT * FROM Asignatura WHERE Código>2 LIMIT 3;
+SELECT * FROM Asignatura LIMIT 2,3;
