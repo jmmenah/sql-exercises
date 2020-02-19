eaa30dab4f1800a163d7ae5562c0af3d8353dc39
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-02-19 21:07:46 +0100

    Add files via upload

--- Tema7/ejercicios/ejercicio7.4.sql
+++ Tema7/ejercicios/ejercicio7.4.sql
@@ -14,3 +14,19 @@ SELECT Alumno.NIF, CONCAT(Nombre,' ', Apellido1,' ', Apellido2) AS "Nombre Compl
 SELECT Descripción, Fecha FROM Matrícula,Asignatura WHERE (Código=Cod_Asig) AND NIF='11111111A' ORDER BY Fecha DESC;
 -- 8. Muestra los alumnos con nota media mayor o igual que 6
 SELECT Nombre FROM Matrícula,Alumno WHERE (Alumno.NIF=Matrícula.NIF) GROUP BY Matrícula.NIF HAVING(AVG(Nota)) >= 6;
+-- 9. Selecciona todos los datos de las matrículas del curso 3o de ESO ordenadas por nota en forma descendente
+SELECT NIF,Cod_Asig,Nota,Fecha FROM Matrícula,Asignatura WHERE (Código=Cod_Asig) AND Curso='3-ESO';
+-- 10. Selecciona todos los datos de las matrículas del departamento de matemáticas con notas mayores que 5.5
+SELECT NIF,Cod_Asig,Nota,Fecha FROM Matrícula,Asignatura WHERE (Código=Cod_Asig) AND Nota>5.5 AND Descripción='Matemáticas';
+-- 11. Selecciona el nombre del alumno, de la asignatura, la nota y la fecha para todas las matrículas cuya nota sea menor que 5
+SELECT Nombre,Descripción AS "Asignatura",Nota,Fecha FROM Matrícula,Asignatura,Alumno WHERE (Código=Cod_Asig) AND (Alumno.NIF=Matrícula.NIF) AND Nota<5;
+-- 12. Obtén un listado de todos los departamentos y la nota media obtenida en sus respectivas asignaturas ordenados por nota
+SELECT Departamento ,AVG(Nota) AS "Nota media" FROM Matrícula,Asignatura WHERE (Código=Cod_Asig) AND Departamento IS NOT NULL GROUP BY Departamento;
+-- 13. Obtén un listado del nombre de los alumnos y el número de matrículas que tienen ordenados por nombre
+SELECT Nombre, COUNT(Matrícula.NIF) AS "Numero Matriculas" FROM Alumno,Matrícula WHERE Alumno.NIF=Matrícula.NIF GROUP BY Matrícula.NIF;
+-- 14. Muestra el nombre completo (en mayúsculas del alumno que tiene la máxima nota) y la nota
+SELECT UCASE(CONCAT(Nombre,' ', Apellido1,' ', Apellido2)) AS "Nombre completo", Nota FROM Alumno,Matrícula WHERE Alumno.NIF=Matrícula.NIF ORDER BY Nota DESC LIMIT 1;
+-- 15. Muestra los datos de las matrículas de todas las asignaturas que no tengan departamento. Incluye la descripción de la asignatura en lugar de su código
+SELECT NIF,Descripción,Nota,Fecha FROM Matrícula,Asignatura WHERE (Cod_Asig=Código) AND Departamento IS NULL;
+-- 16. Muestra los datos de las matrículas de todas las asignaturas que no tengan departamento. Incluye la descripción de la asignatura en lugar de su código y el nombre completo del alumno en lugar del NIF
+SELECT CONCAT(Nombre,' ', Apellido1,' ', Apellido2) AS "Nombre completo",Descripción,Nota,Fecha FROM Matrícula,Asignatura,Alumno WHERE (Código=Cod_Asig) AND (Alumno.NIF=Matrícula.NIF) AND Departamento IS NULL;
